a1de850c6a21612329372f9f83f0244880c3f110
HanSangYoel <samh2006@naver.com>
2019-09-10 01:04:17 +0900

    2019_09_10_finish

--- app/src/main/java/kr/ac/doowon/honeying/FindIDActivity.java
+++ app/src/main/java/kr/ac/doowon/honeying/FindIDActivity.java
@@ -1,10 +1,15 @@
 package kr.ac.doowon.honeying;
 
+import android.media.Image;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import kr.ac.doowon.honeying.BaseActivity;
 import kr.ac.doowon.honeying.R;
@@ -13,34 +18,54 @@ public class FindIDActivity extends BaseActivity {
 
     private EditText inputemailEdt;
     private LinearLayout okBtn;
+    private ImageView backBtn;
+    private InputMethodManager inputMethodManager;
+    long backPressedTimeInMillis = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_id);
+        bindView();
+        setupEvents();
+        setValues();
     }
 
     @Override
     public void setValues() {
-
+        inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
     }
 
     @Override
     public void setupEvents() {
-
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 
     @Override
     public void bindView() {
         this.inputemailEdt = (EditText)findViewById(R.id.inputemailEdt);
         this.okBtn = (LinearLayout)findViewById(R.id.okBtn);
+        this.backBtn = (ImageView)findViewById(R.id.backBtn);
     }
 
     @Override
     public void onBackPressed() {
-        inputemailEdt.clearFocus();
-        inputemailEdt.setFocusable(false);
-        inputemailEdt.setFocusableInTouchMode(true);
-        inputemailEdt.setFocusable(true);
+        long currentTimeInMillis = System.currentTimeMillis();
+        if (currentTimeInMillis - backPressedTimeInMillis < 2000) {
+//            2초 이내에 백버튼을 다시 눌렀으니 종료해야 함.
+            finish();
+            return;
+        }
+        else {
+//            최초로 백버튼을 눌렀거나, 혹은 2초 이상의 시간이 지난 후에 누름.
+            inputMethodManager.hideSoftInputFromWindow(inputemailEdt.getWindowToken(), 0);
+        }
+        backPressedTimeInMillis = currentTimeInMillis;
+
     }
 }
--- app/src/main/java/kr/ac/doowon/honeying/FindPWActivity.java
+++ app/src/main/java/kr/ac/doowon/honeying/FindPWActivity.java
@@ -1,14 +1,30 @@
 package kr.ac.doowon.honeying;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class FindPWActivity extends BaseActivity {
+
+    long backPressedTimeInMillis = 0;
+    private InputMethodManager inputMethodManager;
+    private EditText inputemailEdt;
+    private EditText inputidEdt;
+    private LinearLayout okBtn;
+    private ImageView backBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_pw);
+        bindView();
+        setupEvents();
+        setValues();
     }
 
     @Override
@@ -18,11 +34,35 @@ public class FindPWActivity extends BaseActivity {
 
     @Override
     public void setupEvents() {
-
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 
     @Override
     public void bindView() {
+        this.inputidEdt = (EditText)findViewById(R.id.inputidEdt);
+        this.inputemailEdt = (EditText)findViewById(R.id.inputemailEdt);
+        this.okBtn = (LinearLayout)findViewById(R.id.okBtn);
+        this.backBtn = (ImageView)findViewById(R.id.backBtn);
+    }
+
+    @Override
+    public void onBackPressed() {
+        long currentTimeInMillis = System.currentTimeMillis();
+        if (currentTimeInMillis - backPressedTimeInMillis < 2000) {
+//            2초 이내에 백버튼을 다시 눌렀으니 종료해야 함.
+            finish();
+            return;
+        }
+        else {
+//            최초로 백버튼을 눌렀거나, 혹은 2초 이상의 시간이 지난 후에 누름.
+            inputMethodManager.hideSoftInputFromWindow(inputemailEdt.getWindowToken(), 0);
+        }
+        backPressedTimeInMillis = currentTimeInMillis;
 
     }
 }
